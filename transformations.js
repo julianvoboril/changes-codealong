06945c5d9e65ff4c8aac5a95f0cd75dca4b55c2d
Julian Voboril <julianvoboril@MacBook-Air-von-Julian.local>
2021-06-24 20:17:14 +0200

    Implement final data transformation functions

--- transformations.js
+++ transformations.js
@@ -55,42 +55,57 @@ export function cleanse(input) {
 
 // Codealong: Data Analysis in Node.js
 export function getInterestedRepeatCustomers(inputHubspot, inputStripe) {
-  return inputHubspot.map((hubspotUser) => {
-    // Identify by first name and last name because
-    // sometimes the email address doesn't match
-    const firstName = hubspotUser['First Name'].split(' ')[0];
-    const lastName = hubspotUser['Last Name'];
-    const mailchimpUser = inputMailchimp.find(
-      (user) => user.firstName === firstName && user.lastName === lastName,
-    );
-    const stripeTransaction = inputStripe.find((transaction) => {
+  inputHubspot = cleanse(inputHubspot);
+  inputStripe = deduplicate(inputStripe);
+
+  return inputHubspot.filter((hubspotUser) => {
+    const stripeTransactions = inputStripe.filter((transaction) => {
       const fName = transaction.name.split(' ')[0];
       const lName = transaction.name.split(' ').pop();
-      return fName === firstName && lName === lastName;
+      return (
+        fName === hubspotUser['First Name'] &&
+        lName === hubspotUser['Last Name']
+      );
     });
-
-    return {
-      hubspotContactId: hubspotUser['Contact Id'],
-      firstName,
-      lastName,
-      hubspotLeadStatus: hubspotUser['Lead Status'],
-      hubspotRegisteredAt: hubspotUser['Registered At'],
-      hubspotTotalValue: hubspotUser['Total Value'],
-      mailchimpEmail: mailchimpUser.emailAddress,
-      mailchimpStatus: mailchimpUser.status,
-      mailchimpAudienceName: mailchimpUser.audienceName,
-      mailchimpSubscribeDate: mailchimpUser.subscribeDate,
-      stripeId: stripeTransaction?.id,
-      stripeEmail: stripeTransaction?.email,
-      stripeCreatedAt: stripeTransaction?.created_at_date,
-    };
+    return (
+      ['Open Offer', 'Current Customer'].includes(hubspotUser['Lead Status']) &&
+      stripeTransactions.length > 1
+    );
   });
 }
 
 export function getTotalValueOfAllCustomers(input) {
-  return input;
+  return [
+    {
+      total: input.reduce((total, user) => {
+        return total + parseInt(user['Total Value']);
+      }, 0),
+    },
+  ];
 }
 
 export function getUsersWithNonMatchingEmails(inputMailchimp, inputStripe) {
-  return [];
+  inputStripe = deduplicate(inputStripe);
+
+  return inputMailchimp
+    .map((user) => {
+      const stripeTransaction = inputStripe.find((transaction) => {
+        const fName = transaction.name.split(' ')[0];
+        const lName = transaction.name.split(' ').pop();
+        return fName === user.firstName && lName === user.lastName;
+      });
+
+      return {
+        mailchimpEmail: user.emailAddress,
+        mailchimpStatus: user.status,
+        mailchimpAudienceName: user.audienceName,
+        mailchimpSubscribeDate: user.subscribeDate,
+        stripeId: stripeTransaction?.id,
+        stripeEmail: stripeTransaction?.email,
+        stripeCreatedAt: stripeTransaction?.created_at_date,
+      };
+    })
+    .filter(
+      (user) => user.stripeEmail && user.mailchimpEmail !== user.stripeEmail,
+    );
 }
